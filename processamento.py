53eb9065126d1400bd2cf5eff0a3bdb07d2d6f3c
dougls <douglimadasilva@gmail.com>
2026-05-21 14:37:01 -0300

    adding linter ruff

---
+++ processamento.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os # Import não utilizado
+import sys
+
+def limpa_dados( df):
+  # Espaçamento errado, variável não utilizada
+  x = 10
+  df_limpo=df.dropna()
+  return  df_limpo
